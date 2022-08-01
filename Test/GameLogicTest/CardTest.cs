80d8eb60d4e667f0a94927115057939b96bbc3fe
Søren Holstebroe <soren@holstebroe.dk>
2022-08-01 21:11:37 +0200

    Tilføjede GameLogic folder og test project

---
+++ Test/GameLogicTest/CardTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace GameLogicTest
+{
+    [TestFixture]
+    public class CardTest
+    {
+        [Test]
+        public void TestMe()
+        {
+            Assert.That("42", Is.EqualTo("42"));
+         
+        }
+    }
+}
