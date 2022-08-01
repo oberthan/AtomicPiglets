f7ea9efe40bb3ea7885bf9bab02d46b8e6fb0c6a
Søren Holstebroe <soren@holstebroe.dk>
2022-08-01 22:04:55 +0200

    Add all card types

--- Assets/GameLogic/GameLogic/Card.cs
+++ Assets/GameLogic/GameLogic/Card.cs
@@ -1,7 +1,100 @@
 ﻿namespace GameLogic
 {
-    public class Card
+    public abstract class Card
     {
-        public string Name { get; set; } = "Hej";
+        public string Name { get; set; } = "Card";
     }
+
+    public class AtomicPigletCard : Card
+    {
+
+    }
+    public class DifuseCard : Card
+    {
+
+    }
+    public class NopeCard : Card
+    {
+
+    }
+    public class ShuffleCard : Card
+    {
+
+    }
+    public class AttackCard : Card
+    {
+
+    }
+    public class SeeTheFutureCard : Card
+    {
+
+    }
+    public class FavorCard : Card
+    {
+
+    }
+    public class CollectionCard : Card
+    {
+
+    }
+
+    public class TacoCard : Card
+    {
+
+    }
+    public class RainbowCard : Card
+    {
+
+    }
+    public class PotatoCard : Card
+    {
+
+    }
+    public class BeirdCard : Card
+    {
+
+    }
+    public class WatermelonCard : Card
+    {
+
+    }
+
+    public class CardCollection
+    {
+        private static Random rnd = new Random();
+        private List<Card> cards = new List<Card>();
+        public void Add(Card card)
+        {
+            cards.Add(card);
+        }
+
+        public void AddNew(int count, Func<Card> createFunc)
+        {
+            for(int i = 0; i < count; i++)
+            {
+                cards.Add(createFunc());
+            }
+        }
+        public void Shuffle()
+        {
+            for (int i = 0; i < cards.Count; i++)
+            {
+                cards[i] = cards[i + rnd.Next(cards.Count - i)];
+            }
+        }
+    }
+
+    public class DiscardPile : CardCollection
+    {
+
+    }
+
+    public class CardDeck
+    {
+        public static CardDeck CreateExplodingKittensLikeGame(int playerCount)
+        {
+
+        }
+    }
+
 }
