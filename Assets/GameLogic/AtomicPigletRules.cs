2c14e6794b046897c80d6afd979e9777a285fb2f
Søren Holstebroe <soren@holstebroe.dk>
2022-08-05 21:09:11 +0200

    Net protokol

--- Assets/GameLogic/AtomicPigletRules.cs
+++ Assets/GameLogic/AtomicPigletRules.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GameLogic
 {
@@ -33,11 +35,39 @@ namespace GameLogic
                     if (hand.Contains<AttackCard>()) yield return new AttackAction(player, hand.PeekFromTop<AttackCard>());
                     if (hand.Contains<ShuffleCard>()) yield return new ShuffleAction(player, hand.PeekFromTop<ShuffleCard>());
                     if (hand.Contains<SeeTheFutureCard>()) yield return new SeeTheFutureAction(player, hand.PeekFromTop<SeeTheFutureCard>());
+                    if (hand.Contains<FavorCard>()) yield return new FavorAction(player, hand.PeekFromTop<FavorCard>());
+                    var twoEqualCardGroups = from card in hand.All
+                                               group card by card.Name into collectionGroups
+                                               where collectionGroups.Count() >= 2
+                                               select collectionGroups.Take(2).ToList();
 
+                    var threeEqualCardGroups = from card in hand.All
+                                             group card by card.Name into collectionGroups
+                                             where collectionGroups.Count() >= 3
+                                             select collectionGroups.Take(3).ToList();
+                    var distinctCards = hand.All.Distinct(new GenericEqualityComparer<Card, string>(x => x.Name)).ToList();
                 }
             }
             yield break;
         }
     }
 
+    public class GenericEqualityComparer<T, U> : IEqualityComparer<T>
+    {
+        private readonly Func<T, U> selector;
+
+        public GenericEqualityComparer(Func<T, U> selector)
+        {
+            this.selector = selector;
+        }
+        public bool Equals(T x, T y)
+        {
+            return EqualityComparer<U>.Default.Equals(selector(x), selector(y));
+        }
+
+        public int GetHashCode(T x)
+        {
+            return EqualityComparer<U>.Default.GetHashCode(selector(x));
+        }
+    }
 }
